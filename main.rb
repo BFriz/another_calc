2a2f67611a1d4948ad8eadad8a31817c32b4460d
Ben Frisby <bfrisb@gmail.com>
2015-05-26 21:35:23 +0100

    finished calc needs some css

--- main.rb
+++ main.rb
@@ -21,23 +21,62 @@ get '/basic_calc' do
   erb :basic_calc  
 end
 get '/power' do
-  @first = params[:first].to_i
-  @powerof = params[:powerof].to_i
-  if @first
-    @results = @first ** @powerof
+  @pfirst = params[:pfirst]
+  @powerof = params[:powerof]
+  if @pfirst
+    @results = @pfirst.to_i ** @powerof.to_i
   end
   erb :power
 end
 get '/sqrt' do
-
+  @first = params[:first]
+  if @first
+    @answer = Math.sqrt(@first.to_i)
+  end
   erb :sqrt
 end
 get '/trip_calc' do
+  @distance = params[:distance].to_f
+  @mpg = params[:mpg].to_f
+  @fppg = params[:fppg].to_f
+  @speed = params[:speed]
+  if @speed
+    @hours = @distance / @speed.to_f
+    @price = (@distance/@mpg)*@fppg
+  end
   erb :trip_calc
 end
+
+
+
 get '/mortgage' do
+  @p = params[:p].to_f
+  @ir = params[:ir].to_f
+  @n = params[:n]
+
+  if @n
+    @mir = (@ir/100)/12
+    @x = (1+@mir)**@n.to_f
+    @mort = @p*@mir*@x/(@x -1)
+end
   erb :mortgage
 end
+
+
+
+
 get '/bmi' do
+  @m_or_i = params[:m_or_i]
+  @weight = params[:weight]
+  @height = params[:height]
+  if @m_or_i == "Imperial"
+    @bmi = ((@weight.to_f*703) / (@height.to_f*@height.to_f)).ceil
+  elsif @m_or_i == "Metric"
+    @bmi = (@weight.to_f / (@height.to_f*@height.to_f)).ceil
+  else 
+  end
+    
   erb :bmi
 end
+
+
